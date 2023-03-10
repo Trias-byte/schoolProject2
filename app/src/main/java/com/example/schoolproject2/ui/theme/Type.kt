959acd32a3c254a29cc1e9fdf979210e7dd96212
Trias-home <vasilij5000@mail.ru>
2023-03-10 13:37:17 +0300

    Changing the fonts file extension.

--- app/src/main/java/com/example/schoolproject2/ui/theme/Type.kt
+++ app/src/main/java/com/example/schoolproject2/ui/theme/Type.kt
@@ -2,13 +2,11 @@ package com.example.schoolproject2.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-
 import androidx.compose.ui.unit.sp
 import com.example.schoolproject2.R
 
